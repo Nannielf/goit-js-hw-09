900ab240bc7faa10f9f2e74745e4d288307bed52
Nannielf <135033284+Nannielf@users.noreply.github.com>
2023-11-29 00:01:53 +0000

    Deploying to gh-pages from @ Nannielf/goit-js-hw-09@b87fa577cbabe0db2bdb67183b09545a95113d40 🚀

---
+++ 01-color-switcher.3809e4ad.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=0;e.disabled=!0,e.addEventListener("click",(function(e){t.disabled=!1,e.target.disabled=!0,clearInterval(a),document.body.style.backgroundColor="#fafafa"})),t.addEventListener("click",(function(t){t.target.disabled=!0,e.disabled=!1,a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.3809e4ad.js.map
